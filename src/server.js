a31e461748339508249d773aa26f88f52d8c0178
coderrony <coderronydas@gmail.com>
2026-02-07 15:16:55 +0600

    new change

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const path = require('path');
 // const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
